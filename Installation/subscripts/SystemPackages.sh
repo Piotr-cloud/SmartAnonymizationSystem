ae0ac6661ba1d876cac15d90e4fb8e4199ff47c1
Piotr-cloud <piotrek_osowski@o2.pl>
2023-08-29 14:06:54 +0000

    System packages installation fix

--- Installation/subscripts/SystemPackages.sh
+++ Installation/subscripts/SystemPackages.sh
@@ -6,7 +6,7 @@ sudo apt install build-essential zlib1g-dev libncurses5-dev libgdbm-dev libnss3-
 sudo apt install cmake pkg-config unzip yasm git checkinstall -y || exit $?
 sudo apt install libjpeg-dev libpng-dev libtiff-dev -y || exit $?
 
-sudo apt install libavcodec-dev libavformat-dev libswscale-dev libavresample-dev -y || exit $?
+sudo apt install libavcodec-dev libavformat-dev libswscale-dev -y || exit $?
 sudo apt install libgstreamer1.0-dev libgstreamer-plugins-base1.0-dev -y || exit $?
 sudo apt install libxvidcore-dev x264 libx264-dev libfaac-dev libmp3lame-dev libtheora-dev -y  || exit $?
 sudo apt install libfaac-dev libmp3lame-dev libvorbis-dev -y || exit $?
